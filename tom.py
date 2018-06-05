a0a3b43f33de7c7fc47e175f4765d94490262ff6
Ole Herman Schumacher Elgesem <ole.elgesem@cfengine.com>
2018-06-05 20:03:38 +0200

    Tom can now approve PRs to himself
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@cfengine.com>

--- tom.py
+++ tom.py
@@ -6,27 +6,31 @@ import requests
 import random
 from copy import copy
 
-# Globals for convenience
+# Global constants for convenience
 
 nick = "nickanderson"
 vratislav = "vpodzime"
 craig = "craigcomstock"
 ole = "olehermanse"
 aleksei = "Lex-2008"
+tom = "cf-bottom"
 
 repos = {
-    "core": [ole, vratislav],
-    "enterprise": [ole, vratislav, craig],
-    "nova": [ole, vratislav, craig],
-    "masterfiles": [craig, nick],
-    "buildscripts": [craig, aleksei],
-    "documentation": [nick, craig],
-    "contrib": [nick],
-    "self": [ole, vratislav]
+    "cfengine/core": [ole, vratislav],
+    "cfengine/enterprise": [ole, vratislav, craig],
+    "cfengine/nova": [ole, vratislav, craig],
+    "cfengine/masterfiles": [craig, nick],
+    "cfengine/buildscripts": [craig, aleksei],
+    "cfengine/documentation": [nick, craig],
+    "cfengine/contrib": [nick],
+    "cf-bottom/self": [ole, vratislav, tom]
 }
 
-def get_reviewers(repo, exclude=None):
-    assert exclude is None or type(exclude) is list
+
+def get_maintainers(repo, exclude=None):
+    if not exclude:
+        exclude = []
+    assert type(exclude) is list
 
     defaults = [ole, vratislav]
 
@@ -85,7 +89,7 @@ class GitHub():
 
     def post(self, path, data):
         with open("api.log", "a") as f:
-            f.write("POST {}\n{}".format(path, data))
+            f.write("POST {}\n{}\n".format(path, data))
         r = requests.post(path, headers=self.headers, json=data)
         # print(r.text)
         assert r.status_code >= 200 and r.status_code < 300
@@ -117,13 +121,17 @@ class Comments():
 
 class PR():
     def __init__(self, data, github):
-        self.data = data
-        self.github = github
-        self.comments_url = data["comments_url"]
-        self.author = data["user"]["login"]
-        self.repo = data["base"]["repo"]["name"]
+        self.data = data  # JSON dict from GitHub API
+        self.github = github  # GitHub object with http methods and credentials
+
+        self.comments_url = data["comments_url"]  # POST comments to this URL
+        self.author = data["user"]["login"]  # PR Author / Submitter
+        self.repo = data["base"]["repo"]["full_name"]  # cfengine/core
+
         self.title = data["title"]
         self.number = data["number"]
+        self.api_url = data["url"]
+        self.reviews_url = self.api_url + "/reviews"
 
         self.labels = []
         if "labels" in data:
@@ -131,12 +139,19 @@ class PR():
 
         self.comments = Comments(self.github.get(self.comments_url), github)
 
-        self.reviewers = get_reviewers(self.repo, exclude=[self.author])
+        self.maintainers = get_maintainers(self.repo)
+        self.reviewers = get_maintainers(self.repo, exclude=[self.author])
         if self.author in self.reviewers:
             self.reviewers.remove(self.author)
         if len(self.reviewers) > 1 and nick in self.reviewers:
             self.reviewers.remove(nick)
         self.reviewer = random.choice(self.reviewers)
+
+        self.reviews = self.github.get(self.reviews_url)
+        self.approvals = []
+        for r in self.reviews:
+            if r["state"] == "APPROVED":
+                self.approvals.append(r["user"]["login"])
 
     def has_label(self, label_name):
         label_name = label_name.lower()
@@ -148,18 +163,27 @@ class Tom():
         self.github = GitHub(token)
         self.interactive = interactive
 
-    def comment(self, pr, message):
-        path = pr.comments_url
+    def post(self, path, data, msg=None):
         if self.interactive:
-            print(pretty(pr.data))
-            print("PR: {} ({}#{})".format(pr.title, pr.repo, pr.number))
-            print("Comment: '{}'".format(message))
+            print("I'd like to POST something")
+        else:
+            print("I will POST something")
+        if msg:
+            print(msg)
+        print("Path: {}".format(path))
+        print("Data: {}".format(data))
+        if self.interactive:
             choice = input("Accept? ")
             choice = choice.strip().lower()
             if choice != "y" and choice != "yes":
                 return
-        data = {"body": str(message)}
         self.github.post(path, data)
+
+    def comment(self, pr, message):
+        path = pr.comments_url
+        msg = "PR: {} ({}#{})".format(pr.title, pr.repo, pr.number)
+        data = {"body": str(message)}
+        self.post(path, data, msg)
 
     def ping_reviewer(self, pr):
         if "cf-bottom" in pr.comments.users:
@@ -175,16 +199,29 @@ class Tom():
             comment = "{thanks} for submitting a {pr}! Maybe @{user} can review this?".format(
                 thanks=thanks, pr=pull, user=pr.reviewer)
             self.comment(pr, comment)
-        print("")
+
+    def review(self, pr):
+        if tom not in pr.maintainers:
+            return
+        if tom in pr.approvals:
+            return
+        for person in pr.approvals:
+            if person in pr.maintainers:
+                print("Reviewing: {}".format(pr.title))
+                print("Approved by: " + str(pr.approvals))
+                body = "I trust @{}, approved!".format(person)
+                event = "APPROVE"
+                data = {"body": body, "event": event}
+                self.post(pr.reviews_url, data)
 
     def handle_pr(self, pr):
-        print("Handling PR: {}".format(pr["url"]))
-        # print(pretty(pr))
+        url = pr["url"].replace("https://api.github.com/repos/", "")
+        print("Looking at: {} ({})".format(pr["title"], url))
         pr = PR(pr, self.github)
 
-        # TODO: Add conditions here for posting different types of comments
-        # conditions can be update time, mentions of cf-bottom, etc.
         self.ping_reviewer(pr)
+        self.review(pr)
+        print("")
 
     def run(self):
         self.repos = self.github.get("/user/repos")
@@ -207,8 +244,12 @@ class Tom():
             self.handle_pr(pull)
 
 
-if __name__ == "__main__":
-    args = get_args()
+def run_tom(token, interactive):
+    tom = Tom(token, interactive)
+    tom.run()
+
+
+def get_token():
     token = None
     if "GITHUB_TOKEN" in os.environ:
         token = os.environ["GITHUB_TOKEN"]
@@ -218,9 +259,16 @@ if __name__ == "__main__":
                 token = f.read().strip()
         except (PermissionError, FileNotFoundError):
             pass
+    return token
 
+
+def main():
+    args = get_args()
+    token = get_token()
     if not token:
         sys.exit("Could not get GITHUB_TOKEN from file or env")
+    run_tom(token, args.interactive)
 
-    tom = Tom(token, args.interactive)
-    tom.run()
+
+if __name__ == "__main__":
+    main()  # Don't add any variables to global scope.
