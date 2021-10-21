a5d85f4faf9b9d5812a4143140f3bcb77a99b4f7
Aleksei Shpakovskii <aleksei.shpakovskii@northern.tech>
2021-10-21 13:37:57 +0200

    special case for processing docs prs and repos
    Ticket: ENT-7583

--- tom/jenkins.py
+++ tom/jenkins.py
@@ -41,7 +41,7 @@ class Jenkins():
         except:
             return r.headers, r.text
 
-    def trigger(self, prs: dict[str, int] = None, branch="master", title=None, exotics=False, user=None):
+    def trigger(self, prs: dict[str, int] = None, branch="master", title=None, exotics=False, user=None, docs=False):
         path = self.trigger_url
         params = {}
         branches = ["{}#{}".format(r, p) for r, p in prs.items()]
@@ -52,6 +52,8 @@ class Jenkins():
                 param_name = repo.upper().replace("-", "_")
                 assert " " not in param_name
                 param_name = param_name + "_REV"
+                param_name = param_name.replace('DOCUMENTATION', 'DOCS')
+                param_name = param_name.replace('GENERATOR', 'GEN')
                 params[param_name] = str(prs[repo])
         params["BASE_BRANCH"] = str(branch)
         if not user:
@@ -65,6 +67,21 @@ class Jenkins():
         if exotics:
             description += " - WITH EXOTICS"
         params["BUILD_DESC"] = description
+        if docs:
+            # building documentation
+            if params["BASE_BRANCH"] != 'master':
+                params["BASE_BRANCH"] += '.x'
+            if 'DOCS_REV' not in params:
+                params['DOCS_REV'] = str(branch)
+            if 'DOCS_GEN_REV' not in params:
+                params['DOCS_GEN_REV'] = str(branch)
+            params['BUILD_DOCS'] = True
+            params['DOCS_BRANCH'] = 'pr'
+            params['NO_TESTS'] = True
+            params['NO_DEPLOYMENT_TESTS'] = True
+            params['NO_FR_TESTS'] = True
+            params['NO_STATIC_CHECKS'] = True
+            params['CONFIGURATIONS_FILTER'] = 'label == "PACKAGES_HUB_x86_64_linux_ubuntu_16"'
         return self.post(path, params)
 
     def wait_for_queue(self, url):
