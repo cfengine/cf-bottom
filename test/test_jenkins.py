603ef9824b4d4210795b5120d0edc69c6b8cd92b
Craig Comstock <craig.comstock@northern.tech>
2022-06-14 11:21:57 -0500

    add test for existing docs=True builds

--- test/test_jenkins.py
+++ test/test_jenkins.py
@@ -1,3 +1,4 @@
+import copy
 import os
 from pathlib import Path
 from tom.jenkins import Jenkins
@@ -5,15 +6,43 @@ from tom.main import load_config
 from tom.utils import read_json
 
 
-def test_build_path_and_params_baseline():
-    data = {
-        "prs": {"core": 42},
-        "comment": {"author": "test-author"},
-        "docs": False,
-        "no_tests": True,
-        "pr": {"base_branch": "master", "title": "test-pr-title"},
-        "exotics": False,
+# baseline data, mangle for different cases
+data = {
+    "prs": {"core": 42},
+    "comment": {"author": "test-author"},
+    "docs": False,
+    "no_tests": True,
+    "pr": {"base_branch": "master", "title": "test-pr-title"},
+    "exotics": False,
+}
+
+def test_docs_build():
+    _data = copy.deepcopy(data)
+    _data["docs"] = True
+    (path, params) = build_path_and_params(_data)
+    print(params)
+    assert (
+        path
+        == "https://ci.cfengine.com/job/pr-pipeline/buildWithParameters/api/json"
+    )
+    expected = {
+        "CORE_REV": "42",
+        "BASE_BRANCH": "master",
+        "NO_TESTS": True,
+        "BUILD_DOCS": True,
+        "BUILD_DESC": "test-pr-title @test-author (core#42 master) [NO TESTS]",
+        "DOCS_BRANCH": "pr",
+        "DOCS_GEN_REV": "master",
+        "DOCS_REV": "master",
+        "NO_DEPLOYMENT_TESTS": True,
+        "NO_FR_TESTS": True,
+        "NO_STATIC_CHECKS": True,
+        "CONFIGURATIONS_FILTER": "label == \"PACKAGES_HUB_x86_64_linux_ubuntu_16\""
     }
+    assert expected == params
+
+def test_default_build():
+    print(data)
     (path, params) = build_path_and_params(data)
     assert (
         path
