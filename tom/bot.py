ce1830701b3aa65ea75c06e5184a47024019a330
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2024-05-10 14:24:49 +0200

    Always create an instance of Slack for Bot
    The code relies on it in many places even if it doesn't actually
    communicate to Slack in any way (in the interactive mode, for
    example).

--- tom/bot.py
+++ tom/bot.py
@@ -49,38 +49,33 @@ class Bot:
 
         self.github = GitHub(secrets["GITHUB_TOKEN"], self.username, self.jenkins_repos)
 
-        self.slack = None
-        self.dispatcher = None
-        if secrets.get("SLACK_READ_TOKEN"):
-            self.slack = Slack(
-                read_token=secrets.get("SLACK_READ_TOKEN"),
-                bot_token=secrets.get("SLACK_SEND_TOKEN"),
-                app_token=secrets.get("SLACK_APP_TOKEN"),
-                username=self.username,
-                interactive=interactive,
-            )
-            self.dispatcher = CommandDispatcher(self.slack)
+        self.slack = Slack(
+            read_token=secrets.get("SLACK_READ_TOKEN"),
+            bot_token=secrets.get("SLACK_SEND_TOKEN"),
+            app_token=secrets.get("SLACK_APP_TOKEN"),
+            username=self.username,
+            interactive=interactive,
+        )
+        self.dispatcher = CommandDispatcher(self.slack)
 
-            if "create_prs_from_slack" in self.bot_features:
-                self.github_interface = GitHubInterface(
-                    self.github, self.slack, self.dispatcher
-                )
-            if "update_dependencies" in self.bot_features:
-                self.updater = UpdateChecker(
-                    self.github, self.slack, self.dispatcher, "Lex-2008"
-                )
-            if "generate_changelogs" in self.bot_features:
-                self.changelogger = ChangelogGenerator(
-                    self.github, self.slack, self.dispatcher, "Lex-2008"
-                )
-            if "map_packages" in self.bot_features:
-                self.package_mapper = PackageMapper(
-                    self.github, self.slack, self.dispatcher, "Lex-2008"
-                )
-            if "tag_builds" in self.bot_features:
-                self.tagger = Tagger(
-                    self.github, self.slack, self.dispatcher, "Lex-2008"
-                )
+        if "create_prs_from_slack" in self.bot_features:
+            self.github_interface = GitHubInterface(
+                self.github, self.slack, self.dispatcher
+            )
+        if "update_dependencies" in self.bot_features:
+            self.updater = UpdateChecker(
+                self.github, self.slack, self.dispatcher, "Lex-2008"
+            )
+        if "generate_changelogs" in self.bot_features:
+            self.changelogger = ChangelogGenerator(
+                self.github, self.slack, self.dispatcher, "Lex-2008"
+            )
+        if "map_packages" in self.bot_features:
+            self.package_mapper = PackageMapper(
+                self.github, self.slack, self.dispatcher, "Lex-2008"
+            )
+        if "tag_builds" in self.bot_features:
+            self.tagger = Tagger(self.github, self.slack, self.dispatcher, "Lex-2008")
 
     def post(self, path, data, msg=None):
         if self.interactive:
